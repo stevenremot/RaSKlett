4f187faf5851042c8b0958dd439c6a1a06857f61
Steven Remot <remot@c130-09.enst.fr>
2013-06-03 11:48:08 +0200

    Implémentation de CombinatorManager

--- src/compiler/combinators/CombinatorManager.java
+++ src/compiler/combinators/CombinatorManager.java
@@ -1,9 +1,9 @@
 package compiler.combinators;
 
 import java.util.HashMap;
-import compiler.graph.Node;
 
 /**
+ * @brief Enregistre et rend disponible les combinateurs natifs et créés par l'utilisateur
  * 
  * @author lagrange
  *
@@ -15,20 +15,36 @@ public class CombinatorManager {
 	
 	private CombinatorManager() {
 		combinators = new HashMap<String,Combinator>();
+		set("S", new S());
+		set("K", new K());
+		set("I", new I());
 	}
 	
+	/**
+	 * @return l'unique instance de CombinatorManager
+	 */
 	public static CombinatorManager getInstance() {
 		if(instance == null)
 			instance = new CombinatorManager();
 		return instance;
 	}
 	
+	/**
+	 * @param name
+	 * @return le combinateur de nom name
+	 */
 	public Combinator get(String name) {
 		return combinators.get(name);
 	}
 	
-	public void set(String name, Node node) {
-		
+	/**
+	 * Enregistre le combinator combinator de nom name
+	 * 
+	 * @param name
+	 * @param combinator
+	 */
+	public void set(String name, Combinator combinator) {
+		combinators.put(name, combinator);
 	}
 
 }
