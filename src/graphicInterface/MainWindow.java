2fefe72ab13cf552aafdcecc5b65d7b11ba4ce18
Victor Landeau <landeau@c130-16.enst.fr>
2013-06-10 09:37:22 +0200

    N'importe quoi

--- src/graphicInterface/MainWindow.java
+++ src/graphicInterface/MainWindow.java
@@ -178,15 +178,11 @@ public class MainWindow extends JFrame{
         JPanel combinatorPanel = new JPanel(new GridLayout(0, 1));
         Border border = BorderFactory.createTitledBorder("Native combinators");
         combinatorPanel.setBorder(border);    
-//        JCheckBox check;
-//        for (String s : combinators){
-//        	check = new JCheckBox(s);
-//        	combinatorPanel.add(check);
-//        }
-        CombinatorPanel test = new CombinatorPanel(combinators, "test : ", false);
-        CombinatorPanel test2 = new CombinatorPanel(combinators, "test2 : ", true);
-        combinatorPanel.add(test2);
-        combinatorPanel.add(test);
+        JCheckBox check;
+        for (String s : combinators){
+        	check = new JCheckBox(s);
+        	combinatorPanel.add(check);
+        }
         add(combinatorPanel,  BorderLayout.WEST); 
 
         setPreferredSize(new Dimension(800, 600));
