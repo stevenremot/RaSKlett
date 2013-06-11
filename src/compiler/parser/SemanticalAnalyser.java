fcb44ff03e7923f640cec4b4257e7b7e47968716
Steven Remot <remot@c130-09.enst.fr>
2013-06-11 11:40:21 +0200

    Ajout d'une majuscule au nom de lexecialAnalyserTest

--- src/compiler/parser/SemanticalAnalyser.java
+++ src/compiler/parser/SemanticalAnalyser.java
@@ -1,8 +1,25 @@
 package compiler.parser;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * @brief CLasse réalisant l'analyse sémantique du code
+ * 
+ * @author remot
+ *
+ */
 public class SemanticalAnalyser {
- 
-	public SemanticalAnalyser(){
+	private ArrayList<Instruction> instructions;
+	private Stack insStack;
+	private Instruction currentInstruction;
+	
+	public SemanticalAnalyser(ArrayList<Instruction> instructions){
+		this.instructions = instructions;
 		
+		insStack = new Stack();
+		insStack.addAll(instructions);
 	}
+	
+	private 
 }
