7728f5ad94d5bca37982f3b466336b496f9a27d9
Qianqian Chen <qchen@c130-07.enst.fr>
2013-06-11 10:01:50 +0200

    Premier push du parser

---
+++ src/compiler/parser/Instruction.java
@@ -0,0 +1,38 @@
+package compiler.parser;
+
+import java.util.ArrayList;
+
+public class Instruction {
+	private int numberL = 0;
+	private int numberI = 0;
+
+	private ArrayList<String> contentStrings = new ArrayList<String>();
+
+	public int getNumberL() {
+		return numberL;
+	}
+
+	public void setNumberL(int numberL) {
+		this.numberL = numberL;
+	}
+
+	public int getNumberI() {
+		return numberI;
+	}
+
+	public void setNumberI(int numberI) {
+		this.numberI = numberI;
+	}
+
+	public ArrayList<String> getContentStrings() {
+		return contentStrings;
+	}
+
+	public void setContentStrings(ArrayList<String> contentStrings) {
+		this.contentStrings=contentStrings;
+	}
+	
+	public void addContentStrings(String temStr) {
+		this.contentStrings.add(temStr);
+	}
+}
