995750ea656320381dc7357c61176abf7ba8c19e
Alexandre Kazmierowski <alexandre.kazmierowski@telecom-paristech.fr>
2013-06-14 17:23:13 +0200

    début d'implémentation de l'abstracter

--- src/compiler/abstracter/Abstracter.java
+++ src/compiler/abstracter/Abstracter.java
@@ -1,6 +1,9 @@
 package compiler.abstracter;
 
 import compiler.graph.*;
+import compiler.combinators.CombinatorManager;
+import compiler.combinators.Lambda;
+import compiler.combinators.Var;
 
 /**
  * @brief classe réalisant l'abstraction d'une expression
@@ -20,19 +23,58 @@ public class Abstracter {
 		return abstractedGraph;
 	}
 	
+	
 	public void abstractGraph(){
-		
+	// lambda+	
 		
 		Node node = abstractedGraph;
 		
 		// parcours du graphe pour trouver la dernière expression à extraire
-		while(node.getArgument().getCombinator() != null && (node.getArgument().getCombinator().isLambda() || node.getArgument().getCombinator().isVar()))
+		while(node.getArgument().getCombinator() != null && (node.getArgument().getCombinator() instanceof Lambda || node.getArgument().getCombinator() instanceof Var))
 			node = node.getNextNode();
 		
-		NodeField func = NodeFieldFactory.create(node.getFunction().getNode().getArgument().getCombinator());
-		NodeField arg = NodeFieldFactory.create(node.getArgument().getCombinator());
+		
+		if(node.getFunction().getCombinator() == null){
+			NodeField func = NodeFieldFactory.create(node.getFunction().getNode().getArgument().getCombinator());
+			NodeField arg = NodeFieldFactory.create(node.getArgument().getCombinator());
+		} 	
+		
+	}
+	
+	public Node abstraction(Node expression, int level, Var var){
+		
+		// on suppose que expression.getFunction() = null (technique pour éviter de remonter trop loin)
+		Node lastNode = expression.getLastNode();
+		
+		// cas particuliers
+		if(lastNode.equals(expression)){
+	
+		}
+		
+		else if(lastNode.getFunction().getNode().equals(expression)){
+			
+		}
+		
+		else{
+			
+		}
 		
 		
+		return null;
+	}
+	
+	private NodeField abstractNodeField(NodeField nf, int level, Var var){
+
+		CombinatorManager cmanager = CombinatorManager.getInstance();
+		
+		if(nf.getCombinator() != null){
+			if(nf.getCombinator().equals(var))
+				return NodeFieldFactory.create(cmanager.get("I"));
+			else
+				return NodeFieldFactory.create(new Node(NodeFieldFactory.create(cmanager.get("K")),NodeFieldFactory.create(nf.getCombinator())));
+		}
+		else //cas des parenthèses
+			return NodeFieldFactory.create(abstraction(nf.getNode(),level,var));
 		
 	}
 
--- src/compiler/graph/NodeField.java
+++ src/compiler/graph/NodeField.java
@@ -14,5 +14,5 @@ public interface NodeField {
 	
 	public Combinator getCombinator();
 	public Node getNode();
-
+	
 }
