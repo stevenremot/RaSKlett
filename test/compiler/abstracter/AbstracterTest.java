8b1f56455e88bf0bb8a81effa8f3ae71f36df5f0
Alexandre Kazmierowski <alexandre.kazmierowski@telecom-paristech.fr>
2013-06-17 11:58:57 +0200

    Tests de parenthèses

--- test/compiler/abstracter/AbstracterTest.java
+++ test/compiler/abstracter/AbstracterTest.java
@@ -35,5 +35,46 @@ public class AbstracterTest {
 		
 		assertEquals(ret,"S ( S ( K S ) I ) ( K K )");
 	}
+	
+	@Test
+	public void firstAbstractionParenthesisTest(){
+		
+		
+		Var var = new Var("$x");
+		Combinator S = new DummyCombinator("S"); 
+		Combinator K = new DummyCombinator("K");
+		
+		// lambda+ x . S (x K)
+		Node child = new Node(NodeFieldFactory.create(var),NodeFieldFactory.create(K));
+		Node root = new Node(NodeFieldFactory.create(S),NodeFieldFactory.create(child));
+		
+		Abstracter ab = new Abstracter(root,1);
+		Node result = ab.abstraction(root,1,var);
+		String ret = GraphSerializer.serialize(result);
+		
+		assertEquals(ret,"S ( K S ) ( S I ( K K ) )");
+	}
+	
+	@Test
+	public void firstAbstractionParenthesisTest2(){
+		
+		
+		Var var = new Var("$x");
+		Combinator S = new DummyCombinator("S"); 
+		Combinator K = new DummyCombinator("K");
+		
+		// lambda+ x . S (S x K)
+		Node childRoot = new Node(NodeFieldFactory.create(S),NodeFieldFactory.create(var));
+		Node child = new Node(NodeFieldFactory.create(childRoot),NodeFieldFactory.create(K));
+		childRoot.setNextNode(child);
+		Node root = new Node(NodeFieldFactory.create(S),NodeFieldFactory.create(child));
+		
+		Abstracter ab = new Abstracter(root,1);
+		Node result = ab.abstraction(root,1,var);
+		String ret = GraphSerializer.serialize(result);
+		
+		assertEquals(ret,"S ( K S ) ( S ( S ( K S ) I ) ( K K ) )");
+	}
+
 
 }
