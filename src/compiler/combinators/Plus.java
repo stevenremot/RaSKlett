4e2f481236642d805f5e7f62037704eab9796947
Steven Remot <remot@c130-09.enst.fr>
2013-06-18 08:59:32 +0200

    Implémentation formelle de Plus et tests

--- src/compiler/combinators/Plus.java
+++ src/compiler/combinators/Plus.java
@@ -2,7 +2,6 @@ package compiler.combinators;
 
 import compiler.CompilerException;
 import compiler.graph.Node;
-import compiler.graph.NodeField;
 import compiler.graph.NodeFieldFactory;
 import compiler.reducer.Registry;
 
@@ -20,78 +19,19 @@ public class Plus implements Combinator {
 	}
 
 	/**
-	 * + x y = x s y
+	 * + x y = x (S B) y
 	 */
 	@Override
 	public boolean applyReduction(Registry registry) throws CompilerException {
 		Node node1 = registry.getNode();
 		
-		// Recherche de la première opérande
-		NodeField arg1 = node1.getArgument();
+		node1.setFunction(node1.getArgument());
 		
-		if(arg1.getNode() != null) {
-			Node argNode = arg1.getNode().getRoot();
-			
-			Combinator c = argNode.getFunction().getCombinator();
-			
-			Registry r = new Registry();
-			r.setNode(argNode);
-			
-			if(!c.applyReduction(r)) {
-				throw new CompilerException("La première opérande de + doit être un nombre, non pas une expression incomplète");
-			}
-			
-			node1.setArgument(NodeFieldFactory.create(r.getNode().getLastNode()));
-		}
+		Node sb = new Node(
+				NodeFieldFactory.create(new S()),
+				NodeFieldFactory.create(new B()));
 		
-		Number n1;
-		
-		try {
-			n1 = (Number) arg1.getCombinator();
-		}
-		catch(ClassCastException e) {
-			throw new CompilerException("La première opérande de + doit être un nombre");
-		}
-		
-		// Recherche de la seconde
-		Node node2 = node1.getNextNode();
-		
-		if(node2 == null) {
-			return false;
-		}
-		
-		NodeField arg2 = node2.getArgument();
-		
-		if(arg2.getNode() != null) {
-			Node argNode = arg2.getNode().getRoot();
-			
-			Combinator c = argNode.getFunction().getCombinator();
-			
-			Registry r = new Registry();
-			r.setNode(argNode);
-			
-			if(!c.applyReduction(r)) {
-				throw new CompilerException("La seconde opérande de + doit être un nombre, non pas une expression incomplète");
-			}
-			
-			node2.setArgument(NodeFieldFactory.create(r.getNode().getLastNode()));
-		}
-		
-		Number n2;
-		
-		try {
-			n2 = (Number) arg1.getCombinator();
-		}
-		catch(ClassCastException e) {
-			throw new CompilerException("La première opérande de + doit être un nombre");
-		}
-		
-		// Nouveau combinateur
-		node2.setFunction(NodeFieldFactory.create(new I()));
-		node2.setArgument(NodeFieldFactory.create(new Number(n1.getValue() + n2.getValue())));
-		
-		registry.setNode(node2);
-		
+		node1.setArgument(NodeFieldFactory.create(sb));
 		return true;
 	}
 
