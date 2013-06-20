649c586a7c3e7edc7a6674a45c7d7f7c27812c32
Steven Remot <remot@c130-09.enst.fr>
2013-06-20 15:37:30 +0200

    Fin d'implémentation de LazyCall

--- src/compiler/combinators/LazyCall.java
+++ src/compiler/combinators/LazyCall.java
@@ -5,6 +5,17 @@ import compiler.graph.Node;
 import compiler.graph.NodeFieldFactory;
 import compiler.reducer.Registry;
 
+/**
+ * @brief Combinateur permettant l'appel de fonction de manière paresseuse
+ * 
+ * LazyCall reçoit le nom d'une fonction en argument, et insère son graphe
+ * dans le graphe de l'expression au moment de la réduction.
+ * 
+ * C'est ce combinateur qui permet la récursivité
+ * 
+ * @author lagrange
+ *
+ */
 public class LazyCall implements Combinator {
 
 	private String func;
@@ -19,13 +30,22 @@ public class LazyCall implements Combinator {
 
 	@Override
 	public boolean applyReduction(Registry registry) throws CompilerException {
-		// à finir et tester
 		Combinator f = CombinatorManager.getInstance().get(func);
-		Node node = registry.getNode().getLastNode();
-		Node graph = f.getGraph();
-		node.setFunction(NodeFieldFactory.create(graph));
 		
-		return false;
+		// Si la fonction n'existe pas, on considère qu'on est
+		// en train de faire une réduction pendant la définition de la fonction.
+		// On stoppe donc la réduction
+		if(f == null) {
+			return false;
+		}
+		
+		Node node = registry.getNode();
+		Node graph = f.getGraph();
+		node.setFunction(NodeFieldFactory.create(graph.getLastNode()));
+		
+		registry.setNode(graph);
+		
+		return true;
 	}
 
 	@Override
