d296c5010ef9a2b61cabaed19d33d6daf2df3b75
Steven Rémot <remot@telecom-paristech.fr>
2013-06-23 23:42:01 +0200

    Implémentation de head et tail de manière formelle

--- src/compiler/combinators/Head.java
+++ src/compiler/combinators/Head.java
@@ -2,12 +2,11 @@ package compiler.combinators;
 
 import compiler.CompilerException;
 import compiler.graph.Node;
-import compiler.graph.NodeField;
 import compiler.graph.NodeFieldFactory;
 import compiler.reducer.Registry;
 
 /**
- * @brief Combinateur retournant le premier élément d'une paire
+ * Combinateur retournant le premier élément d'une paire
  * 
  * @author steven
  *
@@ -23,33 +22,23 @@ public class Head implements Combinator {
 	public boolean applyReduction(Registry registry) throws CompilerException {
 		Node node1 = registry.getNode();
 		
-		Node argNode = node1.getArgument().getNode();
-		
+		Node argNode = node1.getArgument().getNode().getLastNode();
+
 		if(argNode == null) {
 			throw new CompilerException("head attend en argument une expression contenant un vec et ses valeurs");
 		}
-		
-		argNode = argNode.getRoot();
-		
-		Combinator c = argNode.getFunction().getCombinator();
-		
-		if(!(c instanceof Vector)) {
-			throw new CompilerException("head ne peut être appelé que sur un vecteur");
-		}
-		
-		NodeField head = argNode.getArgument();
-		
-		Node node2 = node1.getNextNode();
-		
-		if(node2 == null) {
-			node1.setFunction(NodeFieldFactory.create(new I()));
-			node1.setArgument(head);
-		}
-		else {
-			node2.setFunction(head);
-			registry.setNode(node2);
-		}
-		
+
+		 Node kNode = new Node(
+                 NodeFieldFactory.create(argNode),
+                 NodeFieldFactory.create(new K())
+         );
+
+        if(node1.getNextNode() != null) {
+            node1.getNextNode().setFunction(NodeFieldFactory.create(kNode));
+        }
+
+        registry.setNode(kNode);
+
 		return true;
 	}
 
